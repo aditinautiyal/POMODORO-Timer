0092fc61e1c24a3db11b009062b411c8281f15f1
Aditi Nautiyal <66437353+aditinautiyal@users.noreply.github.com>
2025-05-04 15:32:55 -0400

    Create pomodoro.h

---
+++ pomodoro.h
@@ -0,0 +1,11 @@
+#ifndef POMODORO_H
+#define POMODORO_H
+
+// Function declarations
+void clearScreen();
+void displayCountdown(int minutes, int seconds);
+void runTimer(int minutes, const char *message);
+void runWorkSession();
+void runBreakSession(char activities[][100], int numActivities);
+
+#endif /* POMODORO_H */
